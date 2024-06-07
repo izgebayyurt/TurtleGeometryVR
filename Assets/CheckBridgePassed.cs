fd80ff83967350c4a380c64c774467ea0e06a7d5
Matthew Patterson <matthew.patterson@maine.edu>
2024-06-07 17:15:50 -0400

    Built prototype landscape

---
+++ Assets/CheckBridgePassed.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckBridgePassed : MonoBehaviour
+{
+    private GameObject Entrance1;
+    private GameObject Entrance2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if 
+    }
+}
